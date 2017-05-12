4cfac76a3a0f458ffeee2e26954e80436c0ecd93
Johan Nordstrom <johan@digitalvillage.se>
2017-05-12 10:10:04 +0200

    Screencast 1.

--- spotify/index.php
+++ spotify/index.php
@@ -2,5 +2,25 @@
 
 require "autoload.php";
 
-$user = new User("John Doe");
-var_dump($user);
+$artist_johnf = new Artist("John Fogerty");
+$artist_johnf->setAge(38);
+$artist_johnf->setGenre("Country");
+$artist_johnf->setPhoto("https://media.npr.org/assets/music/sotd/2010/02/fogerty_wide-199c64cac07c0b134bce15a9ce9597fdc7600549.jpg?s=1400");
+
+$album_revival = new Album(
+	$artist_johnf,
+	"Revival",
+	2007,
+	new Tracks()
+);
+
+$track_wish = new Track($artist_johnf, "Don't You Wish It Was True");
+$track_gunslinger = new Track($artist_johnf, "Gunslinger");
+
+$album_revival->addTrack($track_wish);
+$album_revival->addTrack($track_gunslinger);
+
+echo "Album:";
+var_dump($album_revival);
+echo "Tracks on Album:";
+var_dump($album_revival->getTracks());
