4363380dd732e765df9070452584fd8b9858087e
Johan Nordstrom <johan@digitalvillage.se>
2017-05-12 13:04:08 +0200

    Basic PDO.

--- spotify/index.php
+++ spotify/index.php
@@ -2,23 +2,17 @@
 
 require "autoload.php";
 
-include "library/artists/metallica.php";
-include "library/artists/nirvana.php";
-
-$artists = [
-	$artist_metallica,
-	$artist_nirvana,
-];
-
-echo "<h1>Artists in our Library</h1>";
-foreach ($artists as $artist) {
-	echo "<h2>Artist: " . $artist->getName() . "</h2>";
-	echo "Age: " . $artist->getAge() . "<br>";
-	echo "<h3>Albums</h3>";
-	foreach ($artist->getAlbumList() as $album) {
-		echo "<h4>Album: " . $album->getName() . "</h4>";
-		foreach ($album->getTrackList() as $track) {
-			echo $track->getName() . "<br>";
-		}
-	}
+try {
+	$pdo = new PDO('mysql:host=localhost;dbname=spotify', 'www', '');
+} catch (PDOException $e) {
+	die("Something went very wrong: " . $e->getMessage());
 }
+
+echo "YEY I HAZ DATABASE!<br>";
+
+echo "I QUERY NAOW!<br>";
+$query = $pdo->prepare("select * from artists");
+$query->execute();
+
+$result = $query->fetchAll(PDO::FETCH_ASSOC);
+var_dump($result);
