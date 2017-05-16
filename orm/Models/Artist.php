aeebfe66f7d37b84b842099f5df57d0ad91adb56
Johan Nordstrom <johan@digitalvillage.se>
2017-05-16 16:06:08 +0200

    Start of moving towards MVC-ish design pattern and implementation. Also a _very_ crude router.

--- orm/Models/Artist.php
+++ orm/Models/Artist.php
@@ -11,8 +11,4 @@ class Artist extends Model {
 	public function albums() {
 		return $this->hasMany(Album::class);
 	}
-
-	public function setName($name) {
-		$this->name = $name;
-	}
 }
--- orm/index.php
+++ orm/index.php
@@ -2,44 +2,24 @@
 
 require "bootstrap.php";
 
-// $new_artist = new Artist();
-// $new_artist->name = "Sean Banan";
-// $new_artist->save();
+$routes = [
+	'' => 'Controllers/WelcomeController.php',
+	'artists' => 'Controllers/ArtistController.php',
+	'albums' => 'Controllers/AlbumController.php',
+];
 
-// $artists = Artist::get()->toArray();
-// $specific_artist = Artist::find(1)->toArray();
-// var_dump($specific_artist);
+$page = (isset($_REQUEST['page'])) ? $_REQUEST['page'] : "";
 
-// function die_Sean_Banan_die() {
-// 	$sean_banans = Artist::where('name', 'Sean Banan')->get();
-// 	foreach ($sean_banans as $sean_banan) {
-// 		foreach ($sean_banan->albums as $album) {
-// 			foreach ($album->tracks as $track) {
-// 				$track->delete();
-// 			}
-// 			$album->delete();
-// 		}
-// 		$sean_banan->delete();
-// 	}
+// if (isset($_REQUEST['page'])) {
+// 	$page = $_REQUEST['page'];
+// } else {
+// 	$page = "";
 // }
 
-// die_Sean_Banan_die();
-// echo "YASSS!!!";
-
-// $new_artist = new Artist();
-// $new_artist->setName("Apan Banane");
-// $new_artist->save();
-
-// $artist = Artist::find(12);
-// $artist->setName("Apan Citron");
-// $artist->save();
-
-// $artist = Artist::where('name', 'Apan Banane')->first();
-// if ($artist !== null) {
-// 	$artist->setName("Björne Banan");
-// 	$artist->save();
-// }
-
-$new_artist = new \App\Models\Artist();
-$new_artist->name = "Dr Bombay";
-$new_artist->save();
+if (array_key_exists($page, $routes)) {
+	// ladda sida
+	$controller = $routes[$page];    // Controllers/WelcomeController.php
+	include $controller;
+} else {
+	die("Page could not be found.");
+}
